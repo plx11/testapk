4f9649a5970bc40b648914188d52ab7bcee30287
plx11 <154478821+plx11@users.noreply.github.com>
2026-05-18 06:15:29 +0800

    Upgrade MainActivity to app theme and preview

--- app/src/main/java/com/plx11/testapk/MainActivity.kt
+++ app/src/main/java/com/plx11/testapk/MainActivity.kt
@@ -5,29 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plx11.testapk.ui.theme.TestapkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            TestapkTheme {
+                App()
+            }
         }
     }
 }
 
 @Composable
 fun App() {
-    MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            Box(contentAlignment = Alignment.Center) {
-                Greeting()
-            }
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Box(contentAlignment = Alignment.Center) {
+            Greeting()
         }
     }
 }
@@ -35,4 +36,12 @@ fun App() {
 @Composable
 fun Greeting() {
     Text(text = "Hello from testapk 🚀")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestapkTheme {
+        App()
+    }
 }
